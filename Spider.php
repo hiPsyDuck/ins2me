04ec22b8439a51f85589ce2fdb1a993ec16a74b4
liujiayoung <gdfs1996@qq.com>
2019-05-28 20:12:10 +0800

    bug fix

--- Spider.php
+++ Spider.php
@@ -4,7 +4,7 @@ namespace ins2me;
 
 class Spider
 {
-<<<<<<< HEAD
+
     //本次下载的图片
     public static $imgFileNames = [];
 
@@ -14,14 +14,6 @@ class Spider
     /** 获取图片url  **/
     public function imgSpider($url)
     {
-=======
-    public static $downloadNumber = 0;
-    public static $imgFileNames = [];
-    private static $path = './img/';
-
-    /** 获取图片url  **/
-    public function imgSpider($url) {
->>>>>>> f579f4de9d791d734fa012cab711049dc079d8f2
         $ch = curl_init();
         self::curlSetOpt($ch, $url);
         $result = curl_exec($ch);
@@ -29,22 +21,14 @@ class Spider
         $error = curl_error($ch);
         curl_close($ch);
         if ($httpCode == 200 && $result) {
-<<<<<<< HEAD
-=======
-            echo "数据获取成功，正在提取图片...\n";
->>>>>>> f579f4de9d791d734fa012cab711049dc079d8f2
             return $result;
         }else{
             return $this->imgSpider($url);
         }
     }
 
-<<<<<<< HEAD
-    public function getImgUrl($url)
-    {
-=======
+
     public function getImgUrl($url) {
->>>>>>> f579f4de9d791d734fa012cab711049dc079d8f2
         $htmlContent = self::imgSpider($url);
         if ($htmlContent) {
             /*利用正则表达式得到图片链接*/
@@ -53,29 +37,19 @@ class Spider
             unset($htmlContent);
             if (!empty($match_result[1])) {
                 $imgUrls = array_unique($match_result[1]);
-<<<<<<< HEAD
-=======
-                echo "图片提取成功，正在下载...\n";
->>>>>>> f579f4de9d791d734fa012cab711049dc079d8f2
                 return $imgUrls;
             }
             return false;
         }
     }
 
-<<<<<<< HEAD
+
     private function curlSetOpt(&$ch, $httpUrl)
     {
         curl_setopt($ch, CURLOPT_PROXY, "ip"); //代理服务器地址
         curl_setopt($ch, CURLOPT_PROXYPORT, port); //代理服务器端口
         curl_setopt($ch, CURLOPT_PROXYUSERPWD, "user:pwd"); //http代理认证帐号
         curl_setopt($ch, CURLOPT_PROXYTYPE, 7); //使用http代理模式
-=======
-    private function curlSetOpt(&$ch, $httpUrl) {
-        curl_setopt($ch, CURLOPT_PROXY, 'ip:port'); //使用socks5 代理，填入代理ip和端口
-        curl_setopt($ch, CURLOPT_PROXYTYPE, 7); //socks5
-        curl_setopt($ch, CURLOPT_PROXYUSERPWD, 'user:password'); //代理账号密码（如果有）
->>>>>>> f579f4de9d791d734fa012cab711049dc079d8f2
         curl_setopt($ch, CURLOPT_URL, $httpUrl);
         curl_setopt($ch, CURLOPT_HEADER, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
@@ -83,7 +57,6 @@ class Spider
         curl_setopt($ch, CURLOPT_TIMEOUT, 30);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 0);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-<<<<<<< HEAD
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
     }
 
@@ -131,40 +104,6 @@ class Spider
             self::downloadImage($image);
         }
 //        return self::$imgFileNames;
-=======
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-    }
-
-    /** 下载图片到img目录 **/
-    public function downloadImage($imgUrl) {
-        $ch = curl_init();
-        self::curlSetOpt($ch, $imgUrl);
-        $file = curl_exec($ch);
-        curl_close($ch);
-        $this->saveAsImage($imgUrl, $file);
-    }
-
-    public function saveAsImage($url, $file) {
-        $filename = strchr(pathinfo($url, PATHINFO_BASENAME), '?', true);
-        if (file_exists(self::$path . $filename)) {
-            //如果已存在则删除（用来获取最大尺寸的图片）
-            unlink(self::$path . $filename);
-        }
-        $resource = fopen(self::$path . $filename, 'a');
-        $res = fwrite($resource, $file);
-        if (false !== $res && !in_array($filename, self::$imgFileNames)) {
-            //记录本次下载的图片
-            array_push(self::$imgFileNames, $filename);
-            echo "成功下载 " . ++self::$downloadNumber . " 张...\n";
-        }
-    }
-
-    public function resolveImgFile($images) {
-        foreach ($images as $image) {
-            self::downloadImage($image);
-        }
-        echo "下载完成！";
->>>>>>> f579f4de9d791d734fa012cab711049dc079d8f2
     }
 
 
